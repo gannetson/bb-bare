ca2611eeb5ee9e9eeff3b9cf2bb4e577fe5967b9
José L. Patiño <jose.lpa@gmail.com>
2013-12-09 13:11:36 +0100

    Added BlueBottle URLs.

--- bare_site/bare_site/urls.py
+++ bare_site/bare_site/urls.py
@@ -1,10 +1,16 @@
 from django.conf.urls import patterns, include, url
+from django.conf.urls.i18n import i18n_patterns
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
+from bluebottle.views import HomeView
+
 urlpatterns = patterns('',
+    url(r'^api/users/', include('bluebottle.accounts.urlsapi')),
+    url(r'^api/utils/', include('bluebottle.bluebottle_utils.urlsapi')),
+    url(r'^api/geo/', include('bluebottle.geo.urlsapi')),
     # Examples:
     # url(r'^$', 'bare_site.views.home', name='home'),
     # url(r'^bare_site/', include('bare_site.foo.urls')),
@@ -14,4 +20,11 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+)
+
+urlpatterns += i18n_patterns('',
+    url(r'^accounts/', include('django.contrib.auth.urls', namespace='accounts')),
+    url(r'^$', HomeView.as_view(), name='home'),
+    url(r'^documents/', include('bluebottle.bluebottle_utils.urls')),
+    url(r'^admin/utils/taggit-autocomplete/', include('taggit_autocomplete_modified.urls')),
 )
